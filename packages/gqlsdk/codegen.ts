3213bce9754a8436613d6bcc8953e4f25c4cfe06
yondav <yoni@yondav.us>
2023-03-25 23:32:27 -0400

    chore(packages/gql/sdk): adds config object for gql codegen

---
+++ packages/gqlsdk/codegen.ts
@@ -0,0 +1,24 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: 'https://0wrqsydr.api.sanity.io/v1/graphql/production/default',
+  documents: ['src/**/*.gql'],
+  // generates: {
+  //   './src/generated/': {
+  //     preset: 'client',
+  //   },
+  // },
+  generates: {
+    './src/generated/index.ts': {
+      plugins: ['typescript', 'typescript-operations', 'typescript-graphql-request'],
+      config: {
+        useTypeImports: true,
+        InlineFragmentTypeOptions: 'combine',
+      },
+    },
+  },
+  hooks: {
+    afterAllFileWrite: ['prettier --write'],
+  },
+};
+export default config;
