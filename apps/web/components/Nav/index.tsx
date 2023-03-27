ee5d1f4973b431e96407ad0f4888c6b98f72f397
yondav <yoni@yondav.us>
2023-03-27 01:23:32 -0400

    style(apps/web/components): adds backdropblur, reduces opacity, removes brightness from sticky nav

--- apps/web/components/Nav/index.tsx
+++ apps/web/components/Nav/index.tsx
@@ -11,8 +11,8 @@ interface NavProps {
 const Base = styled(({ ...props }) => (
   <header {...(({ sticky, ...rest }) => rest)(props)}>{props.children}</header>
 ))<NavProps>`
-  ${tw`w-screen h-16 flex items-center transition-all duration-500 before:(content-[''] absolute -top-full right-full w-full h-16 bg-brand-surface opacity-95 brightness-95 transition-all duration-500)`}
-  ${({ sticky }) => sticky && tw`fixed before:(top-0 right-0) z-40`}
+  ${tw`w-screen h-16 flex items-center transition-all duration-500 before:(content-[''] absolute -top-full right-full w-full h-16 bg-brand-surface opacity-60 transition-all duration-500)`}
+  ${({ sticky }) => sticky && tw`fixed before:(top-0 right-0) z-40 backdrop-blur-md`}
 `;
 
 const Inner = styled.div({ ...gutters, ...tw`flex justify-between items-center z-50` });
