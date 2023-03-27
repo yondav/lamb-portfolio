b8a2f6fddc5deb98e37a2ac83ccc79662f96f31b
yondav <yoni@yondav.us>
2023-03-26 23:00:33 -0400

    chore(apps/web/components): work in progress - layout design for info section

--- apps/web/components/Sections/Info.tsx
+++ apps/web/components/Sections/Info.tsx
@@ -1,7 +1,8 @@
 import type { Info } from '@lamb/gqlsdk';
 import { forwardRef } from 'react';
-import tw, { styled } from 'twin.macro';
+import tw, { css, styled, theme } from 'twin.macro';
 
+import { useMediaQuery } from '../../hooks';
 import { cell, wrapper } from '../../styles';
 import Button from '../Button';
 import { Echo } from '../SVG';
@@ -19,12 +20,30 @@ const InfoSectionWrapper = styled.section<{ layout: 'grid' | 'flex' }>(({ layout
   layout === 'flex' && tw`flex flex-col gap-16`,
 ]);
 
-const InfoHeader = styled(H1)(tw`max-w-xs md:max-w-md lg:max-w-2xl`);
+const InfoHeader = styled(H1)(tw`max-w-xs md:max-w-md lg:max-w-xl xl:max-w-2xl`);
 
 const InfoButtonGroup = styled.div(tw`w-fit flex flex-col gap-6 xl:flex-row`);
 
+const InfoGraphicContainer = styled.div(
+  () => css`
+    ${tw`md:(h-full relative overflow-hidden shadow-nav)`}
+    & div {
+      ${tw`absolute left-0 w-screen sm:flex md:(relative w-full) lg:(grid grid-cols-2 gap-0)`}
+      & svg {
+        ${tw`w-full sm:w-1/2`}
+        @media only screen and (min-width: ${theme`screens.lg`}) {
+          ${tw`col-span-1 w-auto`}
+        }
+      }
+    }
+  `
+);
+
 const InfoSection = forwardRef<HTMLElement, InfoProps>(
   ({ title, credentials, sectionId }, ref) => {
+    const smBreak = useMediaQuery(`(min-width: ${theme`screens.sm`})`);
+    const lgBreak = useMediaQuery(`(min-width: ${theme`screens.lg`})`);
+
     return (
       <>
         <InfoSectionWrapper
@@ -33,7 +52,7 @@ const InfoSection = forwardRef<HTMLElement, InfoProps>(
           css={cell({ span: 12 })}
           layout='grid'
         >
-          <InfoSectionWrapper css={cell({ span: 12, md: 7 })} layout='flex'>
+          <InfoSectionWrapper css={cell({ span: 12, lg: 7 })} layout='flex'>
             <InfoHeader>{title}</InfoHeader>
             <InfoButtonGroup>
               <Button variant='primary'>
@@ -56,8 +75,15 @@ const InfoSection = forwardRef<HTMLElement, InfoProps>(
               </Button>
             </InfoButtonGroup>
           </InfoSectionWrapper>
-          <InfoSectionWrapper css={cell({ span: 12, md: 5 })} layout='flex'>
-            <Echo />
+          <InfoSectionWrapper css={cell({ span: 12, lg: 5 })} layout='flex'>
+            <InfoGraphicContainer>
+              <div>
+                <Echo />
+                <Echo hidden={!smBreak} />
+                <Echo hidden={!lgBreak} />
+                <Echo hidden={!lgBreak} />
+              </div>
+            </InfoGraphicContainer>
           </InfoSectionWrapper>
         </InfoSectionWrapper>
       </>
