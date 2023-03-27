b3d42668f6fbc0bc3b58de2f6a632d67a06c921a
yondav <yoni@yondav.us>
2023-03-26 22:58:04 -0400

    refactor(apps/web/components): refactors echo graphic so that shadow happens outside of svg element

--- apps/web/components/SVG/Echo.tsx
+++ apps/web/components/SVG/Echo.tsx
@@ -1,62 +1,92 @@
-export default function Echo() {
+import tw from 'twin.macro';
+
+export default function Echo({ hidden = false }) {
   return (
-    <svg viewBox='0 0 390 415' fill='none' xmlns='http://www.w3.org/2000/svg'>
-      <g filter='url(#filter0_d_138_401)'>
-        <path d='M390 0.851562H0V390.852H390V0.851562Z' fill='#B38F89' />
-        <path
-          d='M390 0.851562H0V390.852C103.434 390.852 202.632 349.762 275.772 276.623C348.911 203.484 390 104.286 390 0.851562Z'
-          fill='#68263A'
-        />
-        <path
-          d='M0 198.898V192.805C50.8933 192.753 99.6874 172.513 135.674 136.526C171.661 100.539 191.902 51.7448 191.953 0.851563L198.047 0.851562C197.989 53.359 177.105 103.699 139.976 140.828C102.848 177.956 52.5075 198.84 0 198.898Z'
-          fill='#B38F89'
-        />
-        <path
-          d='M0 101.398V95.3047C25.0426 95.2789 49.0521 85.3193 66.7599 67.6115C84.4677 49.9037 94.4273 25.8942 94.4531 0.851562L100.547 0.851563C100.515 27.5083 89.911 53.0641 71.0618 71.9133C52.2126 90.7625 26.6568 101.366 0 101.398Z'
-          fill='#B38F89'
-        />
-        <path
-          d='M0 296.398V290.305C76.742 290.221 150.317 259.698 204.582 205.433C258.846 151.168 289.369 77.5935 289.453 0.851563L295.547 0.851562C295.463 79.2097 264.298 154.334 208.891 209.742C153.483 265.15 78.3582 296.315 0 296.398Z'
-          fill='#B38F89'
-        />
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 390 390'
+      css={hidden ? tw`hidden` : {}}
+    >
+      <g id='a' />
+      <g id='b'>
+        <g>
+          <path d='M390,0H0V390H390V0Z' fill='#b38f89' />
+          <path
+            d='M390,0H0V390c103.43,0,202.63-41.09,275.77-114.23C348.91,202.63,390,103.43,390,0Z'
+            fill='#68263a'
+          />
+          <path
+            d='M0,198.05v-6.09c50.89-.05,99.69-20.29,135.67-56.28C171.66,99.69,191.9,50.89,191.95,0h6.09c-.06,52.51-20.94,102.85-58.07,139.98C102.85,177.1,52.51,197.99,0,198.05Z'
+            fill='#b38f89'
+          />
+          <path
+            d='M0,100.55v-6.09c25.04-.03,49.05-9.99,66.76-27.69C84.47,49.05,94.43,25.04,94.45,0h6.09c-.03,26.66-10.64,52.21-29.49,71.06C52.21,89.91,26.66,100.51,0,100.55Z'
+            fill='#b38f89'
+          />
+          <path
+            d='M0,295.55v-6.09c76.74-.08,150.32-30.61,204.58-84.87C258.85,150.32,289.37,76.74,289.45,0h6.09c-.08,78.36-31.25,153.48-86.66,208.89C153.48,264.3,78.36,295.46,0,295.55Z'
+            fill='#b38f89'
+          />
+        </g>
       </g>
-      <defs>
-        <filter
-          id='filter0_d_138_401'
-          x='-12'
-          y='0.851562'
-          width='414'
-          height='414'
-          filterUnits='userSpaceOnUse'
-          colorInterpolationFilters='sRGB'
-        >
-          <feFlood floodOpacity='0' result='BackgroundImageFix' />
-          <feColorMatrix
-            in='SourceAlpha'
-            type='matrix'
-            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
-            result='hardAlpha'
-          />
-          <feOffset dy='12' />
-          <feGaussianBlur stdDeviation='6' />
-          <feComposite in2='hardAlpha' operator='out' />
-          <feColorMatrix
-            type='matrix'
-            values='0 0 0 0 0.0470588 0 0 0 0 0.0745098 0 0 0 0 0.317647 0 0 0 0.12 0'
-          />
-          <feBlend
-            mode='normal'
-            in2='BackgroundImageFix'
-            result='effect1_dropShadow_138_401'
-          />
-          <feBlend
-            mode='normal'
-            in='SourceGraphic'
-            in2='effect1_dropShadow_138_401'
-            result='shape'
-          />
-        </filter>
-      </defs>
     </svg>
+    // <svg viewBox='0 0 390 415' fill='none' xmlns='http://www.w3.org/2000/svg'>
+    //   <g filter='url(#filter0_d_138_401)'>
+    //     <path d='M390 0.851562H0V390.852H390V0.851562Z' fill='#B38F89' />
+    //     <path
+    //       d='M390 0.851562H0V390.852C103.434 390.852 202.632 349.762 275.772 276.623C348.911 203.484 390 104.286 390 0.851562Z'
+    //       fill='#68263A'
+    //     />
+    //     <path
+    //       d='M0 198.898V192.805C50.8933 192.753 99.6874 172.513 135.674 136.526C171.661 100.539 191.902 51.7448 191.953 0.851563L198.047 0.851562C197.989 53.359 177.105 103.699 139.976 140.828C102.848 177.956 52.5075 198.84 0 198.898Z'
+    //       fill='#B38F89'
+    //     />
+    //     <path
+    //       d='M0 101.398V95.3047C25.0426 95.2789 49.0521 85.3193 66.7599 67.6115C84.4677 49.9037 94.4273 25.8942 94.4531 0.851562L100.547 0.851563C100.515 27.5083 89.911 53.0641 71.0618 71.9133C52.2126 90.7625 26.6568 101.366 0 101.398Z'
+    //       fill='#B38F89'
+    //     />
+    //     <path
+    //       d='M0 296.398V290.305C76.742 290.221 150.317 259.698 204.582 205.433C258.846 151.168 289.369 77.5935 289.453 0.851563L295.547 0.851562C295.463 79.2097 264.298 154.334 208.891 209.742C153.483 265.15 78.3582 296.315 0 296.398Z'
+    //       fill='#B38F89'
+    //     />
+    //   </g>
+    //   <defs>
+    //     <filter
+    //       id='filter0_d_138_401'
+    //       x='-12'
+    //       y='0.851562'
+    //       width='414'
+    //       height='414'
+    //       filterUnits='userSpaceOnUse'
+    //       colorInterpolationFilters='sRGB'
+    //     >
+    //       <feFlood floodOpacity='0' result='BackgroundImageFix' />
+    //       <feColorMatrix
+    //         in='SourceAlpha'
+    //         type='matrix'
+    //         values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+    //         result='hardAlpha'
+    //       />
+    //       <feOffset dy='12' />
+    //       <feGaussianBlur stdDeviation='6' />
+    //       <feComposite in2='hardAlpha' operator='out' />
+    //       <feColorMatrix
+    //         type='matrix'
+    //         values='0 0 0 0 0.0470588 0 0 0 0 0.0745098 0 0 0 0 0.317647 0 0 0 0.12 0'
+    //       />
+    //       <feBlend
+    //         mode='normal'
+    //         in2='BackgroundImageFix'
+    //         result='effect1_dropShadow_138_401'
+    //       />
+    //       <feBlend
+    //         mode='normal'
+    //         in='SourceGraphic'
+    //         in2='effect1_dropShadow_138_401'
+    //         result='shape'
+    //       />
+    //     </filter>
+    //   </defs>
+    // </svg>
   );
 }
