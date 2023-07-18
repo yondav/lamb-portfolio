22e0758ca1c752e8c23182479cf267412c70d78a
yondav <yoni@yondav.us>
2023-07-17 22:40:10 -0400

    refactor(web/components): cleans up button styles

--- apps/web/components/Button/index.tsx
+++ apps/web/components/Button/index.tsx
@@ -1,28 +1,39 @@
 import type { ReactNode } from 'react';
 import tw, { styled, css } from 'twin.macro';
 
+import { slideHover } from '../../styles';
+
 interface ButtonProps {
   variant?: 'primary' | 'secondary';
+  size?: 'sm' | 'md';
 }
 
-const StyledButton = styled.button(({ variant }: ButtonProps) => [
-  tw`w-fit relative overflow-hidden px-6 py-3 rounded transition duration-300 font-copy font-bold text-button-sm md:text-button-md lg:text-button-lg active:shadow-inner`,
+const StyledButton = styled.button(({ variant, size = 'md' }: ButtonProps) => [
+  tw`w-fit relative overflow-hidden rounded transition duration-300 font-copy font-bold active:shadow-inner`,
+
+  size === 'sm' && tw`px-3 py-1.5 text-helper`,
+  size === 'md' && tw`px-6 py-3  text-button-sm sm:text-button-md lg:text-button-lg`,
 
   variant === 'primary' &&
-    tw`bg-brand-cerise-100 text-brand-white shadow-button before:(content-[''] absolute top-0 right-full w-full h-full bg-brand-cerise-300 transition-all duration-500) hover:before:right-0`,
+    css`
+      ${tw`bg-brand-cerise-100 text-brand-white shadow-button`}
+      ${slideHover({ type: 'background', color: 'cerise-300', to: 'right' })}
+    `,
 
   variant === 'secondary' &&
     css`
-      ${tw`border border-brand-cerise-100 bg-brand-surface text-brand-cerise-300 hover:border-brand-surface before:(content-[''] absolute top-0 right-full w-1/2 h-full border-b border-brand-cerise-100 transition-all duration-500)  hover:(before:right-1/4 text-brand-cerise-100)`}
+      ${tw`border border-brand-cerise-100 bg-brand-surface text-brand-cerise-300 hover:border-brand-surface`}
+      ${slideHover({ type: 'underline', color: 'cerise-100', to: 'right' })}
     `,
 ]);
 
 export default function Button({
   variant,
+  size,
   children,
 }: ButtonProps & { children: ReactNode }) {
   return (
-    <StyledButton variant={variant}>
+    <StyledButton variant={variant} size={size}>
       <span tw='relative'>{children}</span>
     </StyledButton>
   );
